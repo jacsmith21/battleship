7afa088cbc070e1a7deaefc5bbac6ec69611f757
Jacob Smith <jacob.smith@unb.ca>
2017-03-29 16:28:13 -0300

    Delete ShipStatusIcon.java

--- ShipStatusIcon.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class ShipStatusIcon extends JLabel{
-	String name;
-
-	public ShipStatusIcon(String name){ this.name = name; }
-	
-	public String getName(){ return name; }
-}
